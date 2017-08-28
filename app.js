418f7e7dc5425d5c4e15765aa6e27f5f51c222ab
Mishra <mailto.swagatika.gmail.com>
2017-08-28 11:16:49 +0530

    my commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var app = express();
+app.get('/',function(req,res){
+  res.sendFile(__dirname+'/index.html');
+  });
+
+  var port = process.env.PORT || 8080;
+
+  var server=app.listen(port,function(req,res){
+      console.log("server is listening at port number 8080");
+  });
